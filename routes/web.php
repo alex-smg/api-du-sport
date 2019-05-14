a04ded356d8e280dee6e742d8e1c6dbb363a39bc
unknown <alex.s95120@gmail.com>
2019-05-14 15:05:05 +0200

    add athletes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@
 Route::get('athletes', function (){
     return view('athlete.allathlete');
 });
+Route::post('athletes', function (){
+    return view('athlete.allathlete');
+});
 
 Route::get('/', function () {
     return view('welcome');
