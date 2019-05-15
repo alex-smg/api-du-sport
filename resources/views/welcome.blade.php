96c4531e0df2bb0322b663f8d5970b0387414553
Le Montagner <victorlemonta@gmail.com>
2019-05-15 12:00:16 +0200

    Changes about sass just a clean up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 <!-- resources/views/welcome.blade.php -->
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
@@ -6,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Les bonnes boules</title>
     <link  rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
     <link href="https://fonts.googleapis.com/css?family=Overpass:300,400,600,700,800" rel="stylesheet">
