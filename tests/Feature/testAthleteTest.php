5f13fe51b0c6584ece76b227057ebb6b627c50a2
alex-smg <alex.s95120@gmail.com>
2019-05-27 16:47:45 +0200

    first test

---
+++ tests/Feature/testAthleteTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class testAthleteTest extends TestCase
+{
+    /**
+     * @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function allAthletesTest()
+    {
+        $response = $this->get('api/allathletes');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function findAthleteTest()
+    {
+        $response = $this->get('api/athlete/1');
+
+        $response->assertStatus(200);
+    }
+    /**
+     * @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function postAthleteTest()
+    {
+        $data = [
+            'id'=> 500,
+            'name' => "test",
+            'description' => "This is a test",
+            'equipe_id' => 5,
+            'image' => "https://images.pexels.com/photos/1000084/pexels-photo-1000084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ];
+
+        $response = $this->json('POST', 'api/add/athlete',$data);
+        $response->assertStatus(201);
+    }
+    /**
+     * @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function UpdateAthleteTest()
+    {
+        $data = [
+            'name' => "test",
+            'description' => "This is a test",
+            'equipe_id' => 5,
+            'image' => "https://images.pexels.com/photos/1000084/pexels-photo-1000084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ];
+
+        $response = $this->json('patch', 'api/update/athlete/500',$data);
+        $response->assertStatus(201);
+    }
+
+    /**
+     * @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function deleteAthleteTest()
+    {
+        $response = $this->delete('api/athlete/delete/500');
+
+        $response->assertStatus(200);
+    }
+}
